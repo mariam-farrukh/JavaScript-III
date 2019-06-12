204aad2eef710f3f679e71f54fa266f28dbef923
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-06-12 14:12:57 -0500

    Answered questions about this

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,19 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Binding: When 'this' points to the window in any place. 
+    It's like saying 'this house' to a house in a row of houses, 
+    where we just don't know which house is being talked about.
+
+* 2. Implicit Binding: It's like saying there is the object, so let's go to it.
+    This is when 'this' is replaced by an object. Ex: this.name can be parent.name.
+
+* 3. New Binding: Involves constructor functions creates an object,
+    and we call it with 'new'. 
+    
+* 4. Explicit Binding: Giving a specific object or variable a new context. Something
+    that can't be applied to anything else adn you can use methods on that specific
+    object or variable
 *
 * write out a code example of each explanation above
 */
